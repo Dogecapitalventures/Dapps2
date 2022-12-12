b29e361446e924300dca98f84dadb0ca414e94cc
Md Shamiul alom Sagor <sagor@Mds-MacBook-Pro.local>
2022-12-13 03:24:40 +0600

    Added my project

---
+++ .history/src/utils/imageSrc_20221212023524.js
@@ -0,0 +1,16 @@
+import logo from '../assets/images/logo.png'
+import logoh from '../assets/images/logoheader.png'
+import earn from '../assets/images/earn.png'
+import lose from '../assets/images/lose.png'
+import meterBg from '../assets/images/metrc-bg.png'
+import meter from '../assets/images/meter.png'
+
+export const imageData ={
+    logoImg:logo
+    logoImg1: earn,
+    earn: earn,
+    lose:lose,
+    meterBg:meterBg,
+    meter:meter
+
+}
